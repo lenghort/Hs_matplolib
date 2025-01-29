a4d59ede944ff72548b9b1d5727fac0a661d211f
lenghort <105147959+lenghort@users.noreply.github.com>
2025-01-29 15:22:31 +0700

    plot 3

---
+++ part_2/plot3.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_funtion():
+    x = np.linspace(-3, 3, 30)
+    y = np.linspace(-3, 3, 30)
+    X,Y = np.meshgrid(x,y)
+
+    Z =  np.sin(3*X)*Y
+
+    fig= plt.figure(figsize =(10,6))
+    ax = fig.add_subplot(111, projection='3d')
+
+    surf = ax.plot_surface(X, Y, Z, cmap='inferno', edgecolor='none')
+    fig.colorbar(surf, ax=ax, label='f(x, y) = sin(3*x)*y')
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('f(x, y)')
+    ax.set_title('Surface Plot of f(x, y) = sin(3*x)*y')
+
+    plt.show()
+
+plot_funtion()
